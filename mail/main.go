cce02eb01c6a832daaa85620c4ab6c881a32ce46
rojasleon <rojasleon.dev@gmail.com>
2022-01-22 17:32:56 -0600

    Separate email logic

--- mail/main.go
+++ mail/main.go
@@ -23,28 +23,34 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Listen for created accounts
 	js.Subscribe(userCreated, func(msg *nats.Msg) {
 		msg.Ack()
 
-		from := mail.NewEmail("Example User", "rojasleon.dev@gmail.com")
-		subject := "Sending with Twilio SendGrid is Fun"
-		to := mail.NewEmail("Example User", string(msg.Data))
-		plainTextContent := "and easy to do anywhere, even with Go"
-		htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
-		message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-		client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
-		response, err := client.Send(message)
-		if err != nil {
-			log.Println(err)
-		} else {
-			fmt.Println(response.StatusCode)
-			fmt.Println(response.Body)
-			fmt.Println(response.Headers)
-		}
+		// Send a welcoming message
+		sendEmail("Welcome!", string(msg.Data))
+
 		// TODO: Why MONITOR?
 	}, nats.Durable("MONITOR"), nats.ManualAck())
 
 	r := gin.Default()
 
 	log.Fatal(r.Run(":8001"))
+}
+
+func sendEmail(subject, address string) {
+	from := mail.NewEmail("Boss", "rojasleon.dev@gmail.com")
+	to := mail.NewEmail("User", address)
+	plainTextContent := "and easy to do anywhere, even with Go"
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
 }
